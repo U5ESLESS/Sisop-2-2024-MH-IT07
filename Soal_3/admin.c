0d075ee9a5fc183c45d8c49eca490accb5575b76
rheca <105469409+rchesa21@users.noreply.github.com>
2024-04-26 16:21:21 +0700

    Update admin.c

--- Soal_3/admin.c
+++ Soal_3/admin.c
@@ -21,7 +21,7 @@ void log_activity(char *user, char *activity, int status) {
 
     log_file = fopen(log_filename, "a");
     if (log_file == NULL) {
-        perror("Error opening log file");
+        perror("Gagal membuka file.log!");
         exit(EXIT_FAILURE);
     }
 
@@ -36,28 +36,47 @@ void log_activity(char *user, char *activity, int status) {
     fclose(log_file);
 }
 
+void display_user_activity(char *user) {
+    char log_filename[50];
+    sprintf(log_filename, "%s%s", user, LOG_FILE_EXTENSION);
+    FILE *log_file = fopen(log_filename, "r");
+    if (log_file == NULL) {
+        perror("Gagal membuka file.log!");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[LOG_MAX_LENGTH];
+    printf("Aktivitas pengguna %s:\n", user);
+    while (fgets(line, LOG_MAX_LENGTH, log_file) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(log_file);
+}
+
 void monitor(char *user) {
+//    printf("Memulai Monitoring %s\n...", user);
     char log_filename[50];
     sprintf(log_filename, "%s%s", user, LOG_FILE_EXTENSION);
     FILE *log_file = fopen(log_filename, "a");
     if (log_file == NULL) {
-        perror("Error opening log file");
+        perror("Gagal membuka file.log!");
         exit(EXIT_FAILURE);
     }
 
-    fprintf(log_file, "Monitoring: %s\n", user);
+    fprintf(log_file, "Memulai Monitoring %s\n", user);
     fflush(log_file);
 
     while (1) {
         pid_t pid = fork();
 
         if (pid == -1) {
-            perror("Error forking process");
+            perror("Error!");
             exit(EXIT_FAILURE);
         } else if (pid == 0) {
             dup2(fileno(log_file), STDOUT_FILENO);
             execlp("ps", "ps", "-u", user, NULL);
-            perror("Error executing ps command");
+            perror("Eksekusi Gagal!");
             exit(EXIT_FAILURE);
         } else {
             wait(NULL);
@@ -69,8 +88,22 @@ void monitor(char *user) {
     fclose(log_file);
 }
 
+void stop_monitor(char *user) {
+    char log_filename[50];
+    sprintf(log_filename, "%s%s", user, LOG_FILE_EXTENSION);
+    FILE *log_file = fopen(log_filename, "a");
+    if (log_file == NULL) {
+        perror("Gagal membuka file.log!");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(log_file, "Stop Monitoring %s\n", user);
+    fclose(log_file);
+    exit(EXIT_SUCCESS);
+}
+
 void block(char *user) {
-    printf("Memblokir proses untuk pengguna: %s\n", user);
+    printf("Memblokir Proses %s\n", user);
     char cmd[100];
     sprintf(cmd, "pkill -u %s", user);
     system(cmd);
@@ -78,7 +111,7 @@ void block(char *user) {
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
-        printf("Perintah: %s -m <user> | -s <user> | -c <user> | -a <user>\n", argv[0]);
+        printf("Perintah Tersedia : %s -m <Start> | -s <Stop> | -c <Block> | -a <Unblock>\n", argv[0]);
         return 1;
     }
 
@@ -86,15 +119,15 @@ int main(int argc, char *argv[]) {
 
     if (strcmp(argv[1], "-m") == 0) {
         monitor(user);
+        log_activity(user, "Memulai Monitoring...", 0);
     } else if (strcmp(argv[1], "-s") == 0) {
-        // kill
-        printf("Fitur stop belum diimplementasikan\n");
+        stop_monitor(user);
     } else if (strcmp(argv[1], "-c") == 0) {
         block(user); // block
         log_activity(user, "Menggagalkan proses", 0); 
     } else if (strcmp(argv[1], "-a") == 0) {
         // unblock
-        printf("Fitur unblock belum diimplementasikan\n");
+        printf("Invalid! Belum selesai.\n");
     } else {
         printf("Perintah tidak diketahui!\n");
         return 1;
