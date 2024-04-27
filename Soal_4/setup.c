89c90462deab135fc1802c6d8a70ba32629c0b3a
Kyfaiyya <151041878+Kyfaiyya@users.noreply.github.com>
2024-04-27 17:28:50 +0800

    Rev

--- Soal_4/setup.c
+++ Soal_4/setup.c
@@ -6,136 +6,210 @@
 #include <signal.h>
 #include <errno.h>
 
-#define MAX_LINE_LENGTH 100
-#define MAX_PROCESSES 100
+#define FILENAME "run.txt"
+#define Max_Line 100
+#define Max_App 100
 
-pid_t child_pids[MAX_PROCESSES];
-int num_child_pids = 0;
+pid_t run[Max_App];
+int num_run = 0;
 
-// Fungsi untuk meluncurkan aplikasi dengan jumlah jendela yang ditentukan
-void launch_application(char *app_name, int num_windows) {
-    pid_t pid;
-
-    // Loop sebanyak jumlah jendela yang diinginkan
-    for (int j = 0; j < num_windows; j++) {
-        pid = fork();
-        if (pid == 0) {
-            // Proses anak
-            execlp(app_name, app_name, NULL);
-            // Jika execlp gagal
-            perror("execlp");
-            exit(EXIT_FAILURE);
-        } else if (pid < 0) {
-            // Penanganan error saat fork()
-            perror("fork");
-            exit(EXIT_FAILURE);
-        } else {
-            // Proses induk
-            child_pids[num_child_pids++] = pid;
-        }
+void addProcess(pid_t pid) {
+    if (num_run < Max_App) {
+        run[num_run++] = pid;
     }
 }
 
-// Fungsi untuk membaca dan menjalankan aplikasi dari file konfigurasi
-void launch_from_config(char *config_file) {
+void saveProcesses() {
+    FILE *file = fopen(FILENAME, "w");
+    if (file != NULL) {
+        fprintf(file, "%d\n", num_run);
+        for (int i = 0; i < num_run; i++) {
+            fprintf(file, "%d\n", run[i]);
+        }
+        fclose(file);
+    }
+}
+
+int startApp(char *app_name, int num_app) {
+    pid_t pid;
+
+    for (int j = 0; j < Max_App; j++) {
+        pid = fork();
+        if (pid == 0) {
+            execlp(app_name, app_name, NULL);
+            perror("execlp");
+            exit(0);
+        } else if (pid < 0) {
+            perror("fork");
+            return 1;
+        } else {
+            addProcess(pid);
+        }
+    }
+    return 0;
+}
+
+int launchFromConfig(char *config_file) {
     FILE *file = fopen(config_file, "r");
-    if (!file) {
-        perror("Error opening file");
-        exit(EXIT_FAILURE);
+    if (file == NULL) {
+        printf("Error opening file %s\n", config_file);
+        exit(1);
     }
 
-    char line[MAX_LINE_LENGTH];
+    char line[Max_Line];
+    char *app;
+    int num;
+
     while (fgets(line, sizeof(line), file)) {
-        char app_name[MAX_LINE_LENGTH];
-        int num_windows;
-        sscanf(line, "%s %d", app_name, &num_windows);
-        launch_application(app_name, num_windows);
+        app = strtok(line, " ");
+        num = atoi(strtok(NULL, " \n"));
+
+        for (int i = 0; i < num; i++) {
+            pid_t pid = fork();
+            if (pid == 0) {
+                execlp(app, app, NULL);
+                exit(0);
+            } else {
+                addProcess(pid);
+            }
+        }
     }
 
     fclose(file);
 }
 
-// Fungsi untuk menutup semua proses anak yang berjalan
-void cleanup_child_processes() {
-    for (int i = 0; i < num_child_pids; i++) {
-        if (kill(child_pids[i], SIGTERM) == -1) {
-            // Penanganan error saat mengirim sinyal kill
-            perror("kill");
-            exit(EXIT_FAILURE);
+void terminateApp(char *app) {
+    if (fork() == 0) {
+        execlp("pkill", "pkill", app, NULL);
+        exit(0);
+    }
+    sleep(1);
+}
+
+void terminateFromConfig(const char* filename) {
+    FILE *file = fopen(filename, "r");
+    if (file != NULL) {
+        char app[256];
+        int num;
+        while (fscanf(file, "%s %d", app, &num) == 2) {
+            terminateApp(app);
+        }
+        fclose(file);
+    }
+}
+
+void loadRunningProcesses() {
+    FILE *file = fopen(FILENAME, "r");
+    if (file != NULL) {
+        fscanf(file, "%d", &num_run);
+        fclose(file);
+    }
+}
+
+void launchApps(char *apps[], int num_apps[]) {
+    pid_t pid;
+
+    for (int i = 0; i < Max_App && apps[i] != NULL; i++) {
+        for (int j = 0; j < num_apps[i]; j++) {
+            pid = fork();
+
+            if (pid == 0) {
+                execlp(apps[i], apps[i], NULL);
+                perror("execlp");
+                exit(0);
+            } else {
+                addProcess(pid);
+            }
         }
     }
 }
 
-// Penanganan sinyal SIGINT (Ctrl+C)
-void sigint_handler(int signum) {
-    // Menutup semua proses anak sebelum keluar dari program
-    cleanup_child_processes();
-    exit(EXIT_SUCCESS);
+void readConfig(char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error: Cannot open file %s\n", filename);
+        exit(1);
+    }
+
+    char line[Max_Line];
+    char *app;
+    int num;
+
+    while (fgets(line, sizeof(line), file)) {
+        app = strtok(line, " ");
+        num = atoi(strtok(NULL, " \n"));
+
+        for (int i = 0; i < num; i++) {
+            pid_t pid = fork();
+            if (pid == 0) {
+                execlp(app, app, NULL);
+                perror("execlp");
+                exit(0);
+            } else {
+                addProcess(pid);
+            }
+        }
+    }
+
+    fclose(file);
+}
+
+void terminateProcesses() {
+    FILE *file = fopen(FILENAME, "r");
+    if (file != NULL) {
+        int num_processes;
+        fscanf(file, "%d", &num_processes);
+        for (int i = 0; i < num_processes; i++) {
+            int pid;
+            fscanf(file, "%d", &pid);
+            if (kill(pid, SIGTERM) == -1) {
+                perror("Error killing process");
+            }
+        }
+        fclose(file);
+    }
 }
 
 int main(int argc, char *argv[]) {
-    // Menetapkan penanganan sinyal SIGINT
-    signal(SIGINT, sigint_handler);
+    loadRunningProcesses();
 
-    // Memeriksa argumen command line
-    if (argc == 1) {
-        printf("Usage: %s [-o <app1> <num1> <app2> <num2> ... <appN> <numN>] | [-f file.conf] | [-k] | [-k file.conf]\n", argv[0]);
-        return 1;
+    if (argc < 2) {
+        printf("Usage: %s -o <app1> <num1> <app2> <num2> ... <appN> <numN> or %s -f <filename> or %s -k\n", argv[0], argv[0], argv[0]);
+        exit(1);
     }
 
-    // Jika argumen adalah -o
     if (strcmp(argv[1], "-o") == 0) {
-        if (argc < 4 || (argc - 2) % 2 != 0) {
-            printf("Invalid arguments\n");
-            return 1;
-        }
-        for (int i = 2; i < argc; i += 2) {
-            launch_application(argv[i], atoi(argv[i + 1]));
-        }
-    }
+        char *apps[Max_App];
+        int num_apps[Max_App];
+        int numArgs = argc - 3;
 
-    // Jika argumen adalah -f
-    else if (strcmp(argv[1], "-f") == 0) {
+        int i, appIndex = 0;
+        for (i = 2; i < argc; i += 2) {
+            apps[appIndex] = argv[i];
+            num_apps[appIndex] = atoi(argv[i + 1]);
+            appIndex++;
+        }
+        apps[appIndex] = NULL;
+
+        launchApps(apps, num_apps);
+        saveRunApps();
+    } else if (strcmp(argv[1], "-f") == 0) {
         if (argc != 3) {
-            printf("Invalid arguments\n");
-            return 1;
+            printf("Usage: %s -f <filename>\n", argv[0]);
+            exit(1);
         }
-        launch_from_config(argv[2]);
-    }
-
-    // Jika argumen adalah -k
-    else if (strcmp(argv[1], "-k") == 0) {
-        if (argc == 2) {
-            // Menutup semua proses
-            cleanup_child_processes();
-            return 0;
-        } else if (argc == 3 && strcmp(argv[2], "-f") != 0) {
-            // Menutup proses sesuai dengan file konfigurasi
-            cleanup_child_processes();
-            return 0;
-        } else if (argc == 4 && strcmp(argv[2], "-f") == 0) {
-            // Menutup proses sesuai dengan file konfigurasi
-            launch_from_config(argv[3]);
-            return 0;
+        readConfig(argv[2]);
+        saveRunApps();
+    } else if (strcmp(argv[1], "-k") == 0) {
+        if (argc == 3) {
+            terminateFromConfig(argv[2]);
         } else {
-            printf("Invalid arguments\n");
-            return 1;
+            terminateProcesses();
+            remove(FILENAME);
         }
-    }
-
-    // Argumen tidak valid
-    else {
-        printf("Invalid arguments\n");
-        return 1;
-    }
-
-    // Menunggu semua proses anak selesai
-    for (int i = 0; i < num_child_pids; i++) {
-        // Menunggu proses anak dan menangani error jika ada
-        if (wait(NULL) == -1 && errno != ECHILD) {
-            perror("wait");
-            exit(EXIT_FAILURE);
-        }
+    } else {
+        printf("Invalid option\n");
+        exit(1);
     }
 
     return 0;
